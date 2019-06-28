ecdc5efe98076536e31b996a4ddabdf5c376814d
Brandon C <b.cline1@yahoo.com>
2019-06-27 20:09:47 -0500

    Organised stuff, got rid of some useless lines

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,5 @@
-/*
-Standard libs for i/o and random number generation
-use std::io;
-use std::cmp::Ordering;
-use rand::Rng;
-*/
+//Standard libs for i/o
+//use std::io;
 
 #[allow(dead_code)]
 #[allow(non_snake_case)]
@@ -14,8 +10,6 @@ mod voice_rec;
 mod help;
 use crate::input_output::*;
 use crate::help::*;
-
-// This program is to be run on a Raspberry Pi, providing a voice interface for a Granny Pod. -Brandon
 
 fn main () {
 	loop {
